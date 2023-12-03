7650838477e4619c5569eddc565a2371eea344cb
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-03 21:35:09 +0200

    add, update, and remove

--- contacts.js
+++ contacts.js
@@ -1,71 +1,59 @@
 const fs = require("fs/promises");
 
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join("./db/contacts.json");
-console.log(contactsPath);
 
-const getAll = async () => {
+const getAllContacts = async () => {
   const data = await fs.readFile(contactsPath);
 
   return JSON.parse(data);
 };
 
 const getContactById = async (id) => {
-  const contacts = await getAll();
+  const contacts = await getAllContacts();
   const result = contacts.find((item) => item.id === id);
   return result || null;
 };
 
-module.exports = {
-  getAll,
-  getContactById,
+const addNewContact = async (data) => {
+  const contacts = await getAllContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 };
-// import { promises as fs } from "fs";
-// import path from "path";
 
-// const contactsPath = path.join(__dirname, "db", "contacts.json");
+const updateContactById = async (id, data) => {
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
 
-// console.log(contactsPath);
+const removeContactById = async (id) => {
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
-// const listContacts = async () => {
-//   try {
-//     const data = await fs.readFile(contactsPath, "utf-8");
-//     const contacts = JSON.parse(data);
-//     return contacts;
-//   } catch (error) {
-//     throw new Error("Unable to read contacts");
-//   }
-// };
-
-// const getContactById = async;
-
-// const addContact = async (contact) => {
-//   try {
-//     const contacts = await listContacts();
-//     contacts.push(contact);
-//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-//     return "Contact added successfully";
-//   } catch (error) {
-//     throw new Error("Unable to add contact");
-//   }
-// };
-
-// const removeContact = async (contactId) => {
-//   try {
-//     const contacts = await listContacts();
-//     const updateContacts = contacts.filter(
-//       (contact) => contact.id !== contactId
-//     );
-//     await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, 2));
-//     return "Contact removed successfully";
-//   } catch (error) {
-//     throw new Error("Unable to remove contact");
-//   }
-// };
-
-// listContacts().then(console.log).catch(console.error);
-// addContact({ id: 1, name: "Vugar Gasimov", phone: "063-684-02-29" })
-//   .then(console.log)
-//   .catch(console.error);
-// removeContact(1).then(console.log).catch(console.error);
+module.exports = {
+  getAllContacts,
+  getContactById,
+  addNewContact,
+  updateContactById,
+  removeContactById,
+};
--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 const contacts = require("./contacts");
-console.log(contacts);
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "listContacts":
-      const allContacts = await contacts.getAll();
+    case "AllContactsList":
+      const allContacts = await contacts.getAllContacts();
       return console.log(allContacts);
+
     case "getContactById":
       const contact = await contacts.getContactById(id);
       return console.log(contact);
+
+    case "addContact":
+      const newContact = await contacts.addNewContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "updateContactById":
+      const updatedContact = await contacts.updateContactById(id, {
+        name,
+        email,
+        phone,
+      });
+      return console.log(updatedContact);
+
+    case "removeContactById":
+      const removedContact = await contacts.removeContactById(id);
+      return console.log(removedContact);
   }
 };
-// invokeAction({ action: "listContacts" });
-invokeAction({ action: "getContactById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
-// listContacts, getContactById,  addContact, removeContact
+// invokeAction({ action: "AllContactsList" });
+
+// invokeAction({ action: "getContactById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+
+// invokeAction({
+//   action: "addContact",
+//   name: "Clementine Bauch",
+//   email: "Nathan@yesenia.net",
+//   phone: "1-463-123-4447",
+// });
+
+// invokeAction({
+//   action: "updateContactById",
+//   id: "J9Ci1aZN5H1rhoqQi_G3e",
+//   name: "Clementine Bauch",
+//   email: "Clementinebauch@yesenia.com",
+//   phone: "1-463-123-4447",
+// });
+
+// invokeAction({ action: "removeContactById", id: "J9Ci1aZN5H1rhoqQi_G3e" });
+
+// AllContactsList, getContactById,  addContact, updateContactById, removeContactById
