9d3c36410df44f442aa5b35135b7b1ff33c5641b
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-03 22:05:51 +0200

    process argv +

--- index.js
+++ index.js
@@ -27,25 +27,9 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       return console.log(removedContact);
   }
 };
-// invokeAction({ action: "AllContactsList" });
 
-// invokeAction({ action: "getContactById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
-
-// invokeAction({
-//   action: "addContact",
-//   name: "Clementine Bauch",
-//   email: "Nathan@yesenia.net",
-//   phone: "1-463-123-4447",
-// });
-
-// invokeAction({
-//   action: "updateContactById",
-//   id: "J9Ci1aZN5H1rhoqQi_G3e",
-//   name: "Clementine Bauch",
-//   email: "Clementinebauch@yesenia.com",
-//   phone: "1-463-123-4447",
-// });
-
-// invokeAction({ action: "removeContactById", id: "J9Ci1aZN5H1rhoqQi_G3e" });
-
-// AllContactsList, getContactById,  addContact, updateContactById, removeContactById
+const actionIndex = process.argv.indexOf("--action");
+if (actionIndex !== -1) {
+  const action = process.argv[actionIndex + 1];
+  invokeAction({ action });
+}
